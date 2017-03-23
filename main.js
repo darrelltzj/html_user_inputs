a12a6dcc1baa06489f5a247079606b5366802814
Darrell Teo <darrelltzj@gmail.com>
2017-03-23 11:24:03 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  var $form = $('form')
+
+  // var $links = $('a')
+  //prevent default flow of html tag
+  // $links.on('click',function (e) {
+  //   e.preventDefault()
+  // })
+
+  $form.on('submit', function (e) {
+    e.preventDefault()
+    var inputsArr = $(this).serializeArray()
+    var outputArr = inputsArr.map(function (input) {
+      return input.value
+    })
+    $('h1').text(outputArr.join(', '))
+  })
+})
